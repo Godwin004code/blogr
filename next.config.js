b994e8e16bd271c3ef33e17c1311bf7486bd2f1b
Alugbin Boluwatife Godwin <godwinalugbin004@gmail.com>
2022-05-26 01:08:54 +0100

    push

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["res.cloudinary.com",'localhost']
+    domains: ["res.cloudinary.com",'localhost', "murmuring-dawn-44285.herokuapp.com"]
   }
 }
 
